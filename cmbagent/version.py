712bb53d51bb92afb0f263f26500502ef8f2a37a
borisbolliet <boris.bolliet@gmail.com>
2024-12-14 14:42:35 +0100

    sync changes, version numbers only

--- cmbagent/version.py
+++ cmbagent/version.py
@@ -12,5 +12,5 @@ __version__: str
 __version_tuple__: VERSION_TUPLE
 version_tuple: VERSION_TUPLE
 
-__version__ = version = '0.1.dev183+g7d4ac83.d20241121'
-__version_tuple__ = version_tuple = (0, 1, 'dev183', 'g7d4ac83.d20241121')
+__version__ = version = '0.1.dev198+g452166d.d20241214'
+__version_tuple__ = version_tuple = (0, 1, 'dev198', 'g452166d.d20241214')
