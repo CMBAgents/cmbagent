8bb746359c4727cd10b654c7ffd081ab8bc37b6f
borisbolliet <boris.bolliet@gmail.com>
2025-03-15 11:41:09 +0000

    logo moved and finance example

--- cmbagent/__init__.py
+++ cmbagent/__init__.py
@@ -13,9 +13,31 @@ set_and_get_rag_data()
 
 
 def print_cmbagent_logo():
-    base_dir = os.path.dirname(os.path.dirname(__file__))
+    base_dir = os.path.dirname(__file__)
     png_path = os.path.join(base_dir, "logo.png")
+    # print(png_path)
+    # print(base_dir)
     display(Image(filename=png_path, width=IMG_WIDTH))
     print(LOGO)
+    # ANSI hyperlink: Note that support varies by terminal.
+    # HTML link for GitHub with icon
+    # github_html = '''
+    # <a href="https://github.com/CMBAgents/cmbagent" target="_blank" style="text-decoration: none;">
+    #   <img src="https://github.githubassets.com/images/modules/logos_page/GitHub-Mark.png" 
+    #        alt="GitHub Icon" width="20" style="vertical-align:middle; margin-right:5px;">
+    #   Star me on GitHub
+    # </a>
+    # '''
+    # # HTML link for YouTube with icon
+    # youtube_html = '''
+    # <a href="https://www.youtube.com/@cmbagent" target="_blank" style="text-decoration: none;">
+    #   <img src="https://upload.wikimedia.org/wikipedia/commons/4/42/YouTube_icon_%282013-2017%29.png" 
+    #        alt="YouTube Icon" width="20" style="vertical-align:middle; margin-right:5px;">
+    #   Watch me on YouTube
+    # </a>
+    # '''
+    
+    # display(HTML(github_html))
+    # display(HTML(youtube_html))
 
 print_cmbagent_logo()
