28486704d497d058c94e44229bb16ae3b3408c11
borisbolliet <boris.bolliet@gmail.com>
2025-04-03 12:37:24 -0400

    Create test_engineer.py

---
+++ tests/test_engineer.py
@@ -0,0 +1,35 @@
+import os
+import re
+
+
+os.environ["CMBAGENT_DEBUG"] = "false"
+os.environ["CMBAGENT_DISABLE_DISPLAY"] = "true"
+
+from cmbagent import CMBAgent
+
+
+cmbagent = CMBAgent(agent_llm_configs = {
+            'engineer': {
+                "model": "gemini-2.5-pro-exp-03-25",
+                "api_key": os.getenv("GEMINI_API_KEY"),
+                "api_type": "google"
+                }
+                })
+
+
+task = r"""
+Draw two random numbers from normal distribution, then compute their sum.
+
+Your final answer should be in format: 
+
+####################\n
+RESULT:\n
+<result>\n
+####################\n
+"""
+
+cmbagent.solve(task,
+               max_rounds=50,
+               initial_agent='engineer',
+               mode = "one_shot",
+              )
