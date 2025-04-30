94e7390812210bd3192114f93774da32f4287972
borisbolliet <boris.bolliet@gmail.com>
2025-04-30 23:24:11 +0100

    astropilot update

--- cmbagent/cmbagent.py
+++ cmbagent/cmbagent.py
@@ -1135,6 +1135,7 @@ def planning_and_control(
 
 
 
+
 def one_shot(
             task,
             max_rounds = 50,
@@ -1214,3 +1215,31 @@ def one_shot(
 
 
 
+
+def get_keywords(input_text: str, n_keywords: int = 5, **kwargs):
+    """
+    Get AAS keywords from input text using astropilot.
+
+    Args:
+        input_text (str): Text to extract keywords from
+        n_keywords (int, optional): Number of keywords to extract. Defaults to 5.
+        **kwargs: Additional keyword arguments
+
+    Returns:
+        dict: Dictionary mapping AAS keywords to their URLs
+    """
+    cmbagent = CMBAgent()
+    PROMPT = f"""
+    {input_text}
+    """
+    cmbagent.solve(task="Find the relevant AAS keywords",
+            max_rounds=50,
+            initial_agent='aas_keyword_finder',
+            mode = "one_shot",
+            shared_context={
+            'text_input_for_AAS_keyword_finder': PROMPT,
+            'N_AAS_keywords': n_keywords,
+                            }
+            )
+    aas_keywords = cmbagent.final_context['aas_keywords'] ## here you get the dict with urls
+    return aas_keywords
