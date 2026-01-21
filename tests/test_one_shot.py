5dca34222ef3ce00e4f2b45f01cb00d44a8e478d
Boris Bolliet <boris.bolliet@gmail.com>
2026-01-21 10:18:57 +0000

    legacy tests

--- tests/test_one_shot.py
+++ tests/test_one_shot.py
@@ -10,9 +10,6 @@ Compute the sum of the first 1000 natural numbers.
 results = cmbagent.one_shot(task,
                    max_rounds=20,
                    agent='engineer',
-                   # engineer_model='gpt-4.1-2025-04-14',
-                #    engineer_model='gemini-2.0-flash',
-                   engineer_model="gemini-2.5-pro-preview-03-25",
-                            
+                   engineer_model="gemini-2.5-flash",
                    work_dir="/Users/boris/Desktop/one_shot",
                   )
