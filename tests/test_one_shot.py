f2ee08c6cea18fd5ef96445ae7fa52ad6e9c3349
Boris Bolliet <boris.bolliet@gmail.com>
2026-01-21 11:23:03 +0000

    test template

--- tests/test_one_shot.py
+++ tests/test_one_shot.py
@@ -1,15 +1,16 @@
 import cmbagent
 
 
-
-
-task = r"""
-Compute the sum of the first 1000 natural numbers.
-"""
-
-results = cmbagent.one_shot(task,
-                   max_rounds=20,
-                   agent='engineer',
-                   engineer_model="gemini-2.5-flash",
-                   work_dir="/Users/boris/Desktop/one_shot",
-                  )
+def test_one_shot():
+    task = r"""
+    Compute the sum of the first 1000 natural numbers.
+    """
+    
+    results = cmbagent.one_shot(task,
+                       max_rounds=20,
+                       agent='engineer',
+                       engineer_model="gemini-2.5-flash",
+                       work_dir="/Users/boris/Desktop/one_shot",
+                      )
+    
+    assert results is not None
